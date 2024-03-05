00c7f3128c9fda71651f8d7fbaf8585c00bdab9e
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-03-05 20:28:12 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
-from utils.general import set_logging, get_base_options
+from utils.general import set_logging, get_options
 
 if __name__ == '__main__':
     set_logging(-1)
 
-    train_options = get_base_options(device_indices='0',
-                                     workers=4,
-                                     resume=False)
+    train_options = get_options(resume=False)
     
