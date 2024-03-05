6395d307cddc2bccac5078fdd99488ee9469fa37
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-03-05 12:02:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,28 +1,9 @@
-import argparse
-import os
-import yaml
-import torch
-import random
-import logging
+from utils.general import set_logging, get_base_options
 
-import numpy as np
-import torch.nn as nn
+if __name__ == '__main__':
+    set_logging(-1)
 
-from tqdm import tqdm
-from pathlib import Path
-from copy import deepcopy
-from torch.utils.tensorboard import SummaryWriter
-
-from utils.general import set_logging, get_base_options, get_latest_run, increment_path, \
-    load_yaml, check_file, colorstr
-
-from utils.torch_utils import init_seeds, select_torch_device, restore_rng_state
-from utils.dataset import create_dataloader
-from models import build_model
-
-from test import test
-
-logger = logging.getLogger(__name__)
-
-
-
+    train_options = get_base_options(device_indices='0',
+                                     workers=4,
+                                     resume=False)
+    
