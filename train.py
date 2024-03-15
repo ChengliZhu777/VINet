fc3c58203ea3e9b98cec4d9aa0ef5f7cce1984fb
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-03-15 14:50:18 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,17 @@
 import yaml
+import logging
 import argparse
 
 from pathlib import Path
 
-from utils.general import set_logging, get_options
+from utils.general import set_logging, get_options, check_filepath, colorstr
+
+
+logger = logging.getLogger(__name__)
+
+
+def train(train_opts):
+    pass
 
 
 if __name__ == '__main__':
@@ -20,6 +28,12 @@ if __name__ == '__main__':
             train_ckpt, str(Path(train_ckpt).parent.parent / 'hyp.yaml'), \
             str(Path(train_ckpt).parent.parent / 'model.yaml'), True
     else:
+        train_options.hyp, train_options.model_cfg, train_options.data = check_filepath(train_options.hyp), \
+                                                                         check_filepath(train_options.model_cfg), \
+                                                                         check_filepath(train_options.data)
         train_options.save_dir = increment_path(Path(train_options.project) / train_options.name,
                                                 exist_ok=False)
-        
+
+    logger.info(colorstr('Train Options: ') + str(train_options))
+
+    train(train_options)
