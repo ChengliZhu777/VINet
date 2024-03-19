02a0fb2de0a47484d64f04e8f8bf02e790926e47
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-03-19 22:43:40 +0800

    Create dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,5 @@
+
+def create_dataloader(paths, base_character, workers, image_width, image_height, batch_size, is_train=False,
+                      standard_char_path=None, prefix='Loading Data', rank=-1):
+    pass
+                        
