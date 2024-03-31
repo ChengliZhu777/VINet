c2042ada8aa7551c5ed9e4fef88ef8389687ea76
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-04-01 00:07:05 +0800

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,10 @@
+import os
 import lmdb
+import torch
 import logging
+
+import numpy as np
+import torchvision.transforms as transforms
 
 from PIL import Image
 from torch.utils.data import Dataset
@@ -27,7 +32,7 @@ class LoadImageAndLabels(Dataset):
         self.char_table = char_table
         self.char2id = dict(zip(self.char_table, range(len(self.char_table))))
 
-        self.width, self.height = image_width, image_height
+        self.target_width, self.target_height = image_width, image_height
         self.prefix = prefix
 
         self.env = lmdb.open(path, max_readers=1, readonly=True, lock=False,
@@ -36,6 +41,8 @@ class LoadImageAndLabels(Dataset):
 
         with self.env.begin(write=False) as txn:
             self.num_samples = int(txn.get('num-samples'.encode()))
+
+        self.width, self.height = image_width, image_height
 
     def __len__(self):
         return self.num_samples
@@ -60,29 +67,54 @@ class LoadImageAndLabels(Dataset):
             label = label.lower()
 
         actual_width, actual_height = image.size
-        if actual_width * 1.5 >= actual_height:
-            vert_mark, org2horiz = False, False
-        else:
-            vert_mark, org2horiz = True, True
+        vert_mark = False if actual_width * 1.5 >= actual_height else True
 
-        image = self.resize_image(image, is_vert=vert_mark, is_rotate=org2horiz)
-        label_length, label_ids = self.convert(label)
+        image = self.resize_image(image, is_vert=vert_mark)
+        label_ids = self.convert(label)
 
-        return image, label, label_length, label_ids, self.char2id['END'], int(vert_mark)
+        return image, label, len(label), label_ids, int(vert_mark), self.char2id['END']
 
-    def resize_image(self, image, is_vert, is_rotate):
+    def resize_image(self, image, is_vert):
 
         if is_vert:
             image = image.transpose(Image.ROTATE_90)  # counterclockwise
 
         image = image.resize((self.target_width, self.target_height), self.interpolation)
-      
-def create_dataloader(paths, base_character, workers, image_width, image_height, batch_size, is_train=False,
-                      standard_char_path=None, prefix='Loading Data', rank=-1):
-    with torch_distributed_zero_first(rank):
-        datasets = []
-        for dataset_format, dataset_path in paths.items():
-            datasets.append(LoadImageAndLabels(dataset_path, base_character,
-                                               image_width, image_height, prefix=prefix))              
 
+        image = transforms.ToTensor()(image)
+        image.sub_(0.5).div_(0.5)
 
+        return image
+
+    def convert(self, label):
+        label_ids = []
+        for i in range(len(label) - 1):
+            try:
+                label_ids.append(self.char2id[label[i]])
+            except:
+                label_ids.append(self.char2id['-'])
+
+        return label_ids
+
+    def get_char_table_size(self):
+        return len(self.char2id)
+
+    @staticmethod
+    def collate_fn(batch):
+        images, labels, label_length, label_ids, vert_marks, end_marks = zip(*batch)
+
+        images = torch.cat([img.unsqueeze(0) for img in images], 0)  # dict -> tensor
+        label_length = torch.from_numpy(np.array(label_length)).long()
+        _label_ids = np.zeros([len(label_ids), len(max(label_ids, key=lambda x:len(x))) + 1])  # start from 1
+
+        label_ids_merge, vert_marks_merge = [], []
+
+        for i, j in enumerate(label_ids):
+            _label_ids[i][1:len(j) + 1] = j
+            label_ids_merge.extend(j)
+            label_ids_merge.append(end_marks[i])
+            vert_marks_merge.extend([vert_marks[i]] * (label_length[i] - 1))
+
+        return images, labels, label_length, torch.from_numpy(_label_ids).long(), \
+            torch.from_numpy(np.array(label_ids_merge)).long(), vert_marks, vert_marks_merge
+        
