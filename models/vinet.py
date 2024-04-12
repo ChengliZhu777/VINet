ee34057c963b368038bcc1db45f14bd4a59313bf
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-04-12 14:39:48 +0800

    Update vinet.py

--- models/vinet.py
+++ models/vinet.py
@@ -1,1 +1,10 @@
-import torch
+import torch.nn as nn
+
+from .model_utils import parse_model
+
+
+class VINet(nn.Module):
+    def __init__(self, backbone, decoder):
+        super(VINet, self).__init__()
+        self.encoder = parse_model(backbone)
+      
