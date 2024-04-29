a9a4e82b9f4be212fe710075458c864ec34ac767
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-04-29 15:39:30 +0800

    Create ve_backbone.py

---
+++ models/backbone/ve_backbone.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+from .resnet import ResNet
+from ..model_utils import parse_model
+
+
+class VEBackbone(nn.Module):
+    def __init__(self, config):
+        super(VEBackbone, self).__init__()
+      pass
