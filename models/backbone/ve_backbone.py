869633c1519936fe6d7d896bca6f52fd7cff9bcc
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-07 14:17:03 +0800

    Update ve_backbone.py

--- models/backbone/ve_backbone.py
+++ models/backbone/ve_backbone.py
@@ -7,4 +7,9 @@ from ..model_utils import parse_model
 class VEBackbone(nn.Module):
     def __init__(self, config):
         super(VEBackbone, self).__init__()
-      pass
+
+        self.model, self.save_layers = parse_model(config, module_name='VEBackbone')
+
+    def forward(self, x):
+        return self.model(x)
+        
