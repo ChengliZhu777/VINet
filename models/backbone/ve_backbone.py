e8df4e137483684e471acd55dace51e3c97b8f0d
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-11 10:38:47 +0800

    Update ve_backbone.py

--- models/backbone/ve_backbone.py
+++ models/backbone/ve_backbone.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
-from .resnet import ResNet
 from ..model_utils import parse_model
+from ..orn.models import upgrade_to_orn
 
 
 class VEBackbone(nn.Module):
@@ -10,6 +10,8 @@ class VEBackbone(nn.Module):
 
         self.model, self.save_layers = parse_model(config, module_name='VEBackbone')
 
+        upgrade_to_orn(self.model, num_orientation=4, scale_factor=2,
+                       classifier=None, features=None, invariant_encoding='align')
+
     def forward(self, x):
         return self.model(x)
-        
