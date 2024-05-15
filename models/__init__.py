2891d4e8ad03fdcda84a1f4febd72c1ca1212e45
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-15 22:23:39 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from .builder import build_model
-
-from .vinet import VINet
-from .backbone import VEBackbone
+from .rtlstd import RTLSTD
+from .backbone.resnet import ResNet
+from .region_proposal import PatchRPN
