f603cec9bad5458f1b17be96b88ed2785497b6eb
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-16 17:17:57 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -78,3 +78,33 @@ class ResBlock(nn.Module):
     def forward(self, x):
         return self.blocks(x)
 
+
+class Embeddings(nn.Module):
+    def __init__(self, char_table_size, d_model):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(char_table_size, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)  # batch-size, num_chars, d_model(256)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=7000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2], pe[:, 1::2] = torch.sin(position * div_term), torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, label_embeddings):
+        x = torch.zeros(label_embeddings.shape).cuda()
+        x = x + self.pe[:, :x.size(1)].requires_grad_(False)
+
+        return self.dropout(x)
+        
