88e5a945db5344125855abcb700b05ac6faaa2ac
ChengliZhu777 <154007802+ChengliZhu777@users.noreply.github.com>
2024-05-22 11:56:06 +0800

    Update vinet.py

--- models/vinet.py
+++ models/vinet.py
@@ -8,4 +8,15 @@ class VINet(nn.Module):
     def __init__(self, backbone, decoder):
         super(VINet, self).__init__()
         self.encoder = build_model(backbone)
-      
+        self.rvi_module = build_model(invariant_module)
+        self.recognition_head = build_model(recognizer)
+
+    def forward(self, images, labels_length, labels_ids, vi_conv_feature=None):
+
+        if vi_conv_feature is None:
+            ve_conv_feature = self.backbone(images)
+            vi_conv_feature = self.rvi_module(ve_conv_feature)
+        else:
+            ve_conv_feature = None
+
+        decoder_output, attention_map = self.recognition_head(labels_ids, vi_conv_feature)[0]
